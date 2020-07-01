03ee366c30c241674162d49b0727f4b9e86d1f80
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2020-07-01 13:44:28 -0400

    Added aws sdk with polly to create mp3 file

--- main.go
+++ main.go
@@ -3,12 +3,46 @@ package main
 import (
 	"encoding/csv"
 	"fmt"
+	"io"
 	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/polly"
 )
 
 type Word struct {
 	english string
 	french  string
+}
+
+func synthesizeSpeech(svc *polly.Polly, word Word, c chan string) {
+	input := &polly.SynthesizeSpeechInput{
+		OutputFormat: aws.String("mp3"),
+		Text:         aws.String(word.french),
+		VoiceId:      aws.String("Celine"),
+	}
+	output, err := svc.SynthesizeSpeech(input)
+	if err != nil {
+		c <- fmt.Sprintf("Failed %s. Err: %s", word.english, err)
+		panic(err)
+	}
+
+	outFile, err := os.Create(fmt.Sprintf("mp3s/%s.mp3", word.english))
+	if err != nil {
+		c <- fmt.Sprintf("Failed %s. Err: %s", word.english, err)
+		panic(err)
+	}
+
+	defer outFile.Close()
+
+	_, err = io.Copy(outFile, output.AudioStream)
+	if err != nil {
+		c <- fmt.Sprintf("Failed %s. Err: %s", word.english, err)
+		panic(err)
+	}
+
+	c <- fmt.Sprintf("Success: %s", word.english)
 }
 
 func main() {
@@ -23,10 +57,23 @@ func main() {
 		panic(err)
 	}
 
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	svc := polly.New(sess)
+
+	c := make(chan string)
+
 	for _, line := range csvLines {
 		word := Word{line[0], line[1]}
-		fmt.Println(word)
+		go synthesizeSpeech(svc, word, c)
+	}
+
+	for i := 0; i < len(csvLines); i++ {
+		fmt.Printf("Completed: %s\n", <-c)
 	}
 
 	defer csvFile.Close()
+	fmt.Println("DONE")
 }
