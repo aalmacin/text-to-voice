5243d65263120f91a3f3353206ad14c6c6147941
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2020-07-01 11:48:12 -0400

    Added csv reading

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+)
+
+type Word struct {
+	english string
+	french  string
+	tag     string
+}
+
+func main() {
+	csvFile, err := os.Open("input.csv")
+
+	if err != nil {
+		panic(err)
+	}
+
+	csvLines, err := csv.NewReader(csvFile).ReadAll()
+	if err != nil {
+		panic(err)
+	}
+
+	for _, line := range csvLines {
+		word := Word{line[0], line[1], line[2]}
+		fmt.Println(word)
+	}
+
+	defer csvFile.Close()
+}
